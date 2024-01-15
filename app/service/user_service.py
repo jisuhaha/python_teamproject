108cfd74d090a51d2f8c667978147d4eff0fdad8
Jisu Han <sysyell3@naver.com>
2024-01-15 17:06:47 +0900

    rege user

--- app/service/user_service.py
+++ app/service/user_service.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import render_template, request
+from app.db import DB
 
 def user_main_service():
     return render_template('/user/main.html')
@@ -7,7 +8,27 @@ def user_login_service():
     return render_template('/user/login.html')
 
 def user_join_service():
-    return render_template('/user/join.html')
+    if request.method=='GET':
+        SQL = 'SELECT oid, groupname FROM XGROUP WHERE STATUS = \'a\''
+        conn = DB('dict')
+        groups = conn.select_all(SQL,None)
+        return render_template('/user/join.html',groups=groups)
+    else:
+        memberid=request.form.get("memberid")
+        password=request.form.get("password")
+        telphone=request.form.get("telphone")
+        groupid=request.form.get("groupid")
+        name=request.form.get("name")
+        carnum=request.form.get("carnum")
+        carinfo=request.form.get("carinfo")
+        status=request.form.get("status")
+        SQL = '''INSERT INTO XMEMBER 
+        (memberid, PASSWORD, telphone, groupid, NAME, carnum, carinfo, STATUS, groupname, createdate)
+        VALUES('{0}','{1}','{2}',{3},'{4}','{5}','{6}','{7}', (select groupname from xgroup where oid={3}), now() )'''.format(memberid, password, telphone, groupid, name, carnum, carinfo, status)
+        print(SQL)
+        conn = DB('dict')
+        groups = conn.save_one(SQL,None)
+        return render_template('/user/join.html')
 
 def user_profile_service():
     return render_template('/user/profile.html')
--- schema.sql
+++ schema.sql
@@ -60,7 +60,7 @@ DROP TABLE IF EXISTS xMember;
 create table xMember(
     oid         INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     memberid    varchar(16),
-    pssword		varchar(256),
+    password	varchar(256),
     groupName	varchar(24),
     groupid		INT,
     telphone	varchar(13),

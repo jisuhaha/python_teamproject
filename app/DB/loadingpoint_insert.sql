b5fc37be82e4c527b9e247aa919e2155a0f54b81
rickymanx4 <rickymanx4@naver.com>
2024-01-16 00:02:49 +0900

    #MJ02

--- app/DB/loadingpoint_insert.sql
+++ app/DB/loadingpoint_insert.sql
@@ -1,69 +1,69 @@
 insert into loadingpoint
 (name, address)
 values
-('롯데제과', '서울 영등포구 양평동 4가');
+('서울', '서울 영등포구 양평동 4가');
 
 insert into loadingpoint	
 (name, address)
 values
-('인쇄닷컴', '서울 중구 을지로 16길 5-5');
+('서울', '서울 중구 을지로 16길 5-5');
 
 insert into loadingpoint	
 (name, address)
 values
-('CJ제일제당', '인천 중구 신흥동3가');
+('인천', '인천 중구 신흥동3가');
 
 insert into loadingpoint	
 (name, address)
 values
-('한일시멘트', '인천 서구 봉수대로 501번길 51');
+('인천', '인천 서구 봉수대로 501번길 51');
 
 insert into loadingpoint	
 (name, address)
 values
-('한솔제지', '대전 대덕구 신일동');
+('대전', '대전 대덕구 신일동');
 
 insert into loadingpoint	
 (name, address)
 values
-('KT&G', '대전 대덕구 벚꽃길 71');
+('대전', '대전 대덕구 벚꽃길 71');
 
 insert into loadingpoint	
 (name, address)
 values
-('KCC', '울산 동구 방어진순환도로 30');
+('울산', '울산 동구 방어진순환도로 30');
 
 insert into loadingpoint	
 (name, address)
 values
-('오뚜기', '울산 울주군 삼납읍 반구대로 149');
+('울산', '울산 울주군 삼납읍 반구대로 149');
 
 insert into loadingpoint	
 (name, address)
 values
-('바디프렌드', '부산 강서구 미음산단로 76번길 12');
+('부산', '부산 강서구 미음산단로 76번길 12');
 
 insert into loadingpoint	
 (name, address)
 values
-('농심식품', '부산 강서구 녹산산단 261로 73번길 48');
+('부산', '부산 강서구 녹산산단 261로 73번길 48');
 
 insert into loadingpoint	
 (name, address)
 values
-('SPC삼립', '대구 달성군 논공읍 논공중앙로 54길 7');
+('대구', '대구 달성군 논공읍 논공중앙로 54길 7');
+
+insert into loadingpoint	
+(name, address)
+value
+('대구', '대구 달성군 화원읍 사문진로 384');
 
 insert into loadingpoint	
 (name, address)
 values
-('한일시멘트', '대구 달성군 화원읍 사문진로 384');
+('광주', '광주 광산구 소촌동 580');
 
 insert into loadingpoint	
 (name, address)
 values
-('금호타이어', '광주 광산구 소촌동 580');
-
-insert into loadingpoint	
-(name, address)
-values
-('해태제과', '광주 북구 하서로 222');
+('광주', '광주 북구 하서로 222');

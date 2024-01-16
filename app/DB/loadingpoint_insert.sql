c61ff242c48f9b94f8d85427a19fdcbfb35e8460
rickymanx4 <rickymanx4@naver.com>
2024-01-16 15:08:55 +0900

    add dumpdata and template

---
+++ app/DB/loadingpoint_insert.sql
@@ -0,0 +1,55 @@
+insert into loadingpoint
+values
+(01, '서울', '서울 영등포구 양평동 4가');
+
+insert into loadingpoint	
+values
+(02, '서울', '서울 중구 을지로 16길 5-5');
+
+insert into loadingpoint	
+values
+(03, '인천', '인천 중구 신흥동3가');
+
+insert into loadingpoint	
+values
+(04, '인천', '인천 서구 봉수대로 501번길 51');
+
+insert into loadingpoint	
+values
+(05, '대전', '대전 대덕구 신일동');
+
+insert into loadingpoint	
+values
+(06, '대전', '대전 대덕구 벚꽃길 71');
+
+insert into loadingpoint	
+values
+(07, '울산', '울산 동구 방어진순환도로 30');
+
+insert into loadingpoint	
+values
+(08, '울산', '울산 울주군 삼납읍 반구대로 149');
+
+insert into loadingpoint	
+values
+(09, '부산', '부산 강서구 미음산단로 76번길 12');
+
+insert into loadingpoint	
+values
+(10, '부산', '부산 강서구 녹산산단 261로 73번길 48');
+
+insert into loadingpoint	
+values
+(11, '대구', '대구 달성군 논공읍 논공중앙로 54길 7');
+
+insert into loadingpoint	
+value
+(12, '대구', '대구 달성군 화원읍 사문진로 384');
+
+insert into loadingpoint	
+values
+(13, '광주', '광주 광산구 소촌동 580');
+
+insert into loadingpoint	
+values
+(14, '광주', '광주 북구 하서로 222');
---
+++ app/DB/xGroups_insert.sql
@@ -0,0 +1,38 @@
+insert into xgroup	
+(groupname, status, createdate)
+values
+('CJ대한통운', '가능', '2020-03-10');
+
+
+insert into xgroup	
+(groupname, status, createdate)
+values
+('우체국택배', '가능', '2019-02-05');
+
+insert into xgroup	
+(groupname, status, createdate)
+values
+('롯데택배', '가능', '2018-10-21');
+
+insert into xgroup	
+(groupname, status, createdate)
+values
+('한진택배', '가능', '2017-09-15');
+
+insert into xgroup	
+(groupname, status, createdate)
+values
+('로젠택배', '가능', '2021-05-08');
+
+insert into xgroup	
+(groupname, status, createdate)
+values
+('FedEx', '가능', '2022-08-22');
+
+insert into xgroup	
+(groupname, status, createdate)
+values
+('DHL', '가능', '2023-11-19');
+
+
+

58ee8804f7b9394b7f5e39fea0b45d6b428192f5
Jisu Han <sysyell3@naver.com>
2024-01-17 15:54:50 +0900

    driver

--- app/route/driver_route.py
+++ app/route/driver_route.py
@@ -4,4 +4,6 @@ from app.service.driver_service import *
 driver_page = Blueprint('driver_page', __name__, url_prefix='/driver')
 
 driver_page.add_url_rule("/price", methods=['GET','POST'], view_func=driver_view_service)
-driver_page.add_url_rule("/table", methods=['GET','POST'], view_func=driver_table_service)
+driver_page.add_url_rule("/mytable", methods=['GET','POST'], view_func=driver_mytable_service)
+driver_page.add_url_rule("/assignable", methods=['GET','POST'], view_func=assignable_table_service)
+driver_page.add_url_rule("/assign", methods=['GET','POST'], view_func=driver_assign_service)
